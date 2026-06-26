4cc39e2d9b727b8c9f10a04ecacf637754f7d2c1
Shiomi@Thinkpad <shiomi@dragon.t.u-tokyo.ac.jp>
2026-06-26 23:13:39 +0900

    DECEL_ZONE = 0.075

--- robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
+++ robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
@@ -187,7 +187,7 @@ class HydrusXiDeformationSequencer:
         tau_des = P_GAIN * angle_diff_to_final
         remaining_angle = abs(angle_diff_to_final)
         
-        DECEL_ZONE = 0.10 
+        DECEL_ZONE = 0.075 
         
         if remaining_angle < DECEL_ZONE:
             fade_factor = remaining_angle / DECEL_ZONE
