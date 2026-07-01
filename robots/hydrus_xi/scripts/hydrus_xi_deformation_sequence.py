9876d731581b04fe7c0622da76bc95738c72aa24
Shiomi@Thinkpad <shiomi@dragon.t.u-tokyo.ac.jp>
2026-07-01 14:18:48 +0900

    P_GAIN = 0.1

--- robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
+++ robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
@@ -182,7 +182,7 @@ class HydrusXiDeformationSequencer:
     def _calculate_target_moment(self, joint_name):
         angle_diff_to_final = self._get_angle_difference(self.current_q[joint_name], self.target_q[joint_name])
         
-        P_GAIN = 0.2
+        P_GAIN = 0.1
         MAX_DRIVE_TORQUE_BASE = 0.15
         tau_des = P_GAIN * angle_diff_to_final
         remaining_angle = abs(angle_diff_to_final)
