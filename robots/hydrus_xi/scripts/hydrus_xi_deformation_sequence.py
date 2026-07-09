53881529e42ba008245fee5d86fbd0149c24695b
Shiomi@Thinkpad <shiomi@dragon.t.u-tokyo.ac.jp>
2026-07-09 10:28:47 +0900

    修正した

--- robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
+++ robots/hydrus_xi/scripts/hydrus_xi_deformation_sequence.py
@@ -293,9 +293,9 @@ class HydrusXiDeformationSequencer:
         """
         angle_diff_to_final = self._get_angle_difference(self.current_q['joint1'], self.target_q['joint1'])
         
-        P_GAIN = 0.02
-        MAX_DRIVE_TORQUE_BASE = 0.18
-        MIN_FRICTION_TORQUE = 0.15
+        P_GAIN = 0.03
+        MAX_DRIVE_TORQUE_BASE = 0.25
+        MIN_FRICTION_TORQUE = 0.18
 
         tau_des = P_GAIN * angle_diff_to_final
         remaining_angle = abs(angle_diff_to_final)
