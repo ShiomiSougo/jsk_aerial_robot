2cb8951a77b6b2521f93d1fcbee2f57c5dc5dd46
Shiomi@Thinkpad <shiomi@dragon.t.u-tokyo.ac.jp>
2026-07-10 19:24:57 +0900

    修正した

--- robots/hydrus_xi/include/hydrus_xi/hydrus_xi_under_actuated_navigation.h
+++ robots/hydrus_xi/include/hydrus_xi/hydrus_xi_under_actuated_navigation.h
@@ -4,7 +4,33 @@
  *
  * Copyright (c) 2020, JSK Lab
  * All rights reserved.
- * ...
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/o2r other materials provided
+ * with the distribution.
+ * * Neither the name of the JSK Lab nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  *********************************************************************/
 
 #pragma once
@@ -14,9 +40,8 @@
 #include <hydrus/hydrus_tilted_robot_model.h>
 #include <nlopt.hpp>
 #include <OsqpEigen/OsqpEigen.h>
-// ===== 内部モーメント指令用 =====
-#include <std_msgs/Float64MultiArray.h>
-#include <std_msgs/Float64.h> 
+// ===== 【新規追加】内部モーメント指令用 =====
+#include <std_msgs/Float64MultiArray.h> 
 
 namespace aerial_robot_navigation
 {
@@ -51,19 +76,14 @@ namespace aerial_robot_navigation
 
     void setMaxMinYaw(const double max_min_yaw) { max_min_yaw_ = max_min_yaw;}
 
-    // ===== 内部モーメント制御用パブリックアクセッサー & メソッド =====
+    // ===== 【新規追加】内部モーメント制御用パブリックアクセッサー =====
     inline int getTargetJointIndex() const { return target_joint_index_; }
     inline double getTauDesTarget() const { return tau_des_target_; }
     inline bool hasMomentCommand() const { return has_moment_command_; }
-    
-    double computeExactInternalMoment(
-        const std::vector<double>& x,
-        const boost::shared_ptr<HydrusTiltedRobotModel>& robot_model_ptr);
+    inline double getTargetMomentWeight() const { return target_moment_weight_; }
 
   private:
-    // ★ 【修正】各ジンバルへの個別コマンド送信に変更
-    std::vector<ros::Publisher> gimbal_ctrl_pubs_; 
-    
+    ros::Publisher gimbal_ctrl_pub_;
     std::thread plan_thread_;
     boost::shared_ptr<HydrusTiltedRobotModel> robot_model_for_plan_;
     OsqpEigen::Solver yaw_range_lp_solver_;
@@ -76,13 +96,13 @@ namespace aerial_robot_navigation
 
     bool plan_verbose_;
     bool maximize_yaw_;
-    double force_norm_weight_;
-    double force_variant_weight_;
-    double yaw_torque_weight_;
-    double fc_t_min_weight_;
-    double baselink_rot_thresh_;
-    double fc_t_min_thresh_;
-    double gimbal_delta_angle_;
+    double force_norm_weight_; // cost func
+    double force_variant_weight_; // cost func
+    double yaw_torque_weight_; // cost func
+    double fc_t_min_weight_; // cost func
+    double baselink_rot_thresh_; // constraint func
+    double fc_t_min_thresh_; // constraint func
+    double gimbal_delta_angle_; // configuration state
 
     std::vector<double> opt_gimbal_angles_, prev_opt_gimbal_angles_;
 
@@ -91,15 +111,20 @@ namespace aerial_robot_navigation
 
     void rosParamInit() override;
 
-    // ===== 内部モーメント制御用メンバ変数 =====
-    int target_joint_index_;
-    double tau_des_target_;
-    bool has_moment_command_;
+    // ===== 【新規追加】内部モーメント制御用メンバ変数・メソッド =====
+    int target_joint_index_;              // 対象関節インデックス (0=Joint1, 1=Joint2, 2=Joint3, -1=なし)
+    double tau_des_target_;               // 目標内部モーメント [N⋅m]
+    bool has_moment_command_;             // コマンド受信フラグ
+    double target_moment_weight_;         // ペナルティ重み（ROS パラメータから読み込み）
 
-    ros::Subscriber moment_command_sub_;
+    ros::Subscriber moment_command_sub_;  // /hydrus_xi/target_internal_moment の Subscriber
 
     void momentCommandCallback(const std_msgs::Float64MultiArray::ConstPtr& msg);
     
+    double computeInternalMomentZ(
+        const std::vector<double>& x,
+        const boost::shared_ptr<HydrusTiltedRobotModel>& robot_model_ptr);
+        
     std::vector<double> extractThrustsFromOptVars(
         const std::vector<double>& x,
         const boost::shared_ptr<HydrusTiltedRobotModel>& robot_model_ptr);
@@ -107,4 +132,4 @@ namespace aerial_robot_navigation
     std::vector<double> extractGimbalsFromOptVars(
         const std::vector<double>& x);
   };
-} // namespace aerial_robot_navigation
+};
